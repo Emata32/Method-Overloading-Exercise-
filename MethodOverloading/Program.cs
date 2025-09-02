0d1f95f500c56880ca589b53e3257ab68efdd4af
Eduardo Mata <eddiemata15533@gmail.com>
2025-09-02 15:40:59 -0500

    Finish Method Overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,39 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add (int one, int two)
         {
-            Console.WriteLine("Hello, World!");
+            return one + two;
+        }
+
+        public static decimal Add(decimal one, decimal two)
+        {
+            return one + two;
+        }
+
+        public static string Add(int one, int two, bool isCurrency)
+        {
+            var sum = one + two;
+
+            if (isCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCurrency && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+                
+        }
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1m, 2.5m));
+            Console.WriteLine(Add(1, 0,true));
         }
     }
 }
